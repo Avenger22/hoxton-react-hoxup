da835be9100078525405f7b263abf7a1b1143e3a
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-01-18 13:28:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// #region 'Importing'
+// import { useState } from "react"
+// import { Route, Routes, Navigate } from "react-router-dom"
+import MainMenu from './Pages/MainMenu'
+// import SideChatList from './Pages/SideChatList'
+// import MainMessageList from './Pages/MainMessagesList'
+// import Login from './Pages/Login'
+// #endregion
+
+export default function App() {
+
+  // #region 'Returning Html'
+  return (
+
+      <>
+        <MainMenu />
+  
+      </>
+
+  )
+  // #region 'Returning Html'
+
+}
