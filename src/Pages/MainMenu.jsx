0dd5b1bf379db8094901a114fd8bc7ced64f9f28
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-01-24 13:17:57 +0100

    added a modal but bugs reamin

--- src/Pages/MainMenu.jsx
+++ src/Pages/MainMenu.jsx
@@ -58,7 +58,7 @@ function MainMenu(props) {
         }
        
         return true
-        
+
       })
     
       function createConversation (participantId) {
@@ -185,6 +185,76 @@ function MainMenu(props) {
                 </main>
 
             ) : null}
+
+            {modal === 'start-chat' ? (
+
+                <div className='modal-wrapper'>
+
+                    <div className='modal'>
+
+                        <button className='close-modal' onClick={() => setModal('')}>
+                            X
+                        </button>
+
+                        <h1>Start chat</h1>
+
+                        {/* 
+                        this modal should display all users
+                        I have no conversations with yet ✅
+                        */}
+
+                        {usersIHaveNotTalkedToYet.length > 0 ? (
+
+                        <ul>
+
+                            {usersIHaveNotTalkedToYet.map(user => (
+
+                            <li key={user.id}>
+                                <button
+                                    className='chat-button'
+                                    onClick={() => {
+                                        // clicking on one of those users
+                                        // should start a conversation with them
+                                        // how do we start a conversation?
+                                        // - create a conversation on the server
+                                        // - update conversations state
+                                        createConversation(user.id)
+                                    }}
+                                >
+
+                                <img
+                                    className='avatar'
+                                    height='50'
+                                    width='50'
+                                    alt=''
+                                    src={user.avatar}
+                                />
+
+                                <div>
+                                    <h3>
+                                    {user.firstName} {user.lastName}
+                                    </h3>
+                                </div>
+
+                                </button>
+
+                            </li>
+
+                            ))}
+
+                        </ul>
+
+                        ) : (
+
+                        <p>No new person to talk to</p>
+
+                        )}
+
+                    </div>
+
+                </div>
+
+                ) : null}
 
             </div>
 
